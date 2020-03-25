b80d5d4f12189f26685febf2786f764018d84215
Kofi Ramos Amoussou <karsoft92@gmail.com>
2020-03-25 22:56:48 +0000

    core: category page wip

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -3,13 +3,31 @@
 @section('content')
     @include('templates.partials._global-navigation')
 
-    <div class="hero w-full ">
+    <div class="hero w-full px-4 py-4">
         <div class="w-full bg-white flex">
             <a href="" class="font-bold">Home</a>
             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8"><path d="M9 5l7 7-7 7"></path></svg>
             <a href="" class="font-bold">Category</a>
             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8"><path d="M9 5l7 7-7 7"></path></svg>
             <a href="">sub category</a>
+        </div>
+
+        <div class="w-full">
+            <div class="flex">
+                <div class="md:w-1/3">
+
+                </div>
+                <div class="md:w-2/3">
+
+                </div>
+            </div>
+        </div>
+
+    </div><!--/end of her -->
+
+    <div class="w-full">
+        <div class="flex">
+            di
         </div>
 
     </div>
