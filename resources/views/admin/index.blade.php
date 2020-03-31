71b5ee502a1dd32a89f48fdb008bb454bf5c9ed3
Kofi Ramos Amoussou <karsoft92@gmail.com>
2020-03-31 13:30:23 +0000

    wip

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -4,10 +4,10 @@
 @include('templates.partials._global-navigation')
 <div class="w-full">
     <div class="w-full">
-        <div class="flex">
-            <div class="w-64 bg-gray-700 px-2 h-full text-white">
+        <div class="flex px-4">
+            <div class="w-64 bg-gray-700 px-4  -mx-4 h-full text-white py-5">
                 <header class="text-2xl font-bold">Settings</header>
-                <a href="ml-4">Profile</a>
+                <a href="ml-4 px-4">Profile</a>
                 <a href="ml-4">Passwords</a>
 
                 <header class="text-2xl font-bold">Products</header>
