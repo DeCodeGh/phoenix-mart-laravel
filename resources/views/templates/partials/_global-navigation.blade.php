ddb79c7cfd2d3003f8cd3c5debb84e73ca163a4a
Kofi Ramos Amoussou <karsoft92@gmail.com>
2020-04-13 00:08:50 +0000

    user registration phase 1

--- resources/views/templates/partials/_global-navigation.blade.php
+++ resources/views/templates/partials/_global-navigation.blade.php
@@ -3,6 +3,7 @@
 <nav class="bg-black md:py-6">
       <div class="flex justify-between">
           <div class="hidden sm:block sm:ml-6">
+             <a href="{{route('home')}}" class="ml-4 px-3 py-2 rounded-md text-sm font-medium leading-5 text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Home</a>
             <a href="#" class="ml-4 px-3 py-2 rounded-md text-sm font-medium leading-5 text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Women</a>
             <a href="#" class="ml-4 px-3 py-2 rounded-md text-sm font-medium leading-5 text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Men</a>
             <a href="#" class="ml-4 px-3 py-2 rounded-md text-sm font-medium leading-5 text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Kids</a>
@@ -14,7 +15,7 @@
               <a href="" class="text-gray-400 hover:text-white px-2">User</a>
             @else
               <a href="{{route('login')}}" class="text-gray-400 hover:text-white px-2">Login</a>
-              <a href="{{route('register')}}" class="text-gray-400 hover:text-white px-2">Dashboard</a>
+              <a href="{{route('register')}}" class="text-gray-400 hover:text-white px-2">Register</a>
             @endif
           </div>
       </div>
