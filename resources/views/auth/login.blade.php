4b7a5cffa49ccf13eec1f996278ae2962bfb1ef1
Kofi Ramos Amoussou <karsoft92@gmail.com>
2020-04-19 20:17:04 +0000

    registration page ui closes #15

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,16 +3,16 @@
 @section('content')
 
 <div class="w-full">
-  <h3 class="text-4xl leading-10 w-full tracking-tight font-extrabold text-gray-900 text-center mt-4">Phoenix Mart</h3>
-  <div class="flex  justify-center  bg-white mt-4 bg-gray-50 px-4 mt-4">
-  <form action="{{route('login')}}" method="post" class="bg-gray-300 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+  <h3 class="text-4xl leading-10 tracking-tight font-extrabold text-gray-900 text-center mt-4">Phoenix Mart</h3>
+   <h4 class="text-2xl leading-10 w-full tracking-tight font-bold text-gray-900 text-center mt-4">Welcome Back!</h4>
+  <div class="flex  justify-center  bg-white mt-4 w-full bg-gray-50 px-4 mt-4">
+  <form action="{{route('login')}}" method="post" class="bg-gray-300 shadow-md rounded px-8 pt-6 pb-8 mb-4 md:w-2/5 mx-auto">
      @csrf
       <div class="w-full mt-4">
-        <label for="" class="block text-gray-700 text-sm font-extrabold  text-xl  mb-2">Email</label>
-        <input type="text" name="email" id="" autofocus class=" mt-1 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username">
+        <label for="" class="block text-gray-700  font-extrabold  text-xl  mb-2">Email</label> <input type="text" name="email" id="" autofocus class=" mt-1 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username">
       </div>
        <div class="w-full mt-4">
-        <label for="" class="block text-gray-700 text-sm font-extrabold text-xl  mb-2">Password</label>
+        <label for="" class="block text-gray-700  font-extrabold text-xl  mb-2">Password</label>
         <input type="password" name="password" id="" class="mt-1 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
       </div>
        <div class="w-full mt-4">
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,29 +2,33 @@
 
 @section('content')
 
-<div class="flex  justify-center  bg-white mt-4 px-4 py-16">
-<form action="{{route('register')}}" method="post" class="px-8 py-4 mt-4 border-2 border-gray-400">
-   @csrf
-    <div class="w-full">
-      <label for="" class="block">Name</label>
-      <input type="text" name="name" id="" class="appearance-none mt-4">
-    </div>
-     <div class="w-full">
-      <label for="" class="block">Email</label>
-      <input type="text" name="email" id="" class="appearance-none mt-4">
-    </div>
-     <div class="w-full">
-      <label for="" class="block">Password</label>
-      <input type="password" name="password" id="" class="appearance-none focus:shadow-outline text-black focus:text-gray-600 mt-4">
-    </div>
-     <div class="w-full">
-      <label for="" class="block">Password</label>
-      <input type="password" name="password_confirmation" id="" class="appearance-none focus:shadow-outline text-black focus:text-gray-600 mt-4">
-    </div>
-     <div class="w-full mt-4">
-     <button class="bg-black text-white rounded px-4 py-2">Register</button>
-    </div>
- </form>
+<div class="w-full">
+  <h3 class="text-4xl leading-10 w-full tracking-tight font-extrabold text-gray-900 text-center mt-4">Phoenix Mart</h3>
+  <h4 class="text-2xl leading-10 w-full tracking-tight font-bold text-gray-900 text-center mt-4">Registration</h4>
+  <div class="flex  justify-center  bg-white mt-4 bg-gray-50 px-4 mt-4">
+  <form action="{{route('register')}}" method="post" class="bg-gray-300 shadow-md rounded px-8 pt-6 pb-8 mb-4 md:w-2/5 mx-auto">
+     @csrf
+      <div class="w-full">
+        <label for="" class="block text-gray-700 font-extrabold text-xl mb-2 mt-4">Name</label>
+        <input type="text" name="name" id="" class=" mt-1 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+      </div>
+       <div class="w-full">
+        <label for="" class="block text-gray-700 font-extrabold text-xl mb-2 mt-4">Email</label>
+        <input type="text" name="email" id="" class=" mt-1 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+      </div>
+       <div class="w-full">
+        <label for="" class="block text-gray-700 font-extrabold text-xl mb-2 mt-4">Password</label>
+        <input type="password" name="password" id="" class=" mt-1 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+      </div>
+       <div class="w-full">
+        <label for="" class="block text-gray-700 font-extrabold text-xl mb-2 mt-4">Confirm Password</label>
+        <input type="password" name="password_confirmation" id="" class=" mt-1 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+      </div>
+       <div class="w-full mt-4">
+       <button class="bg-black text-white rounded px-4 py-2">Register</button>
+      </div>
+   </form>
+  </div>
 </div>
 
 @endsection
