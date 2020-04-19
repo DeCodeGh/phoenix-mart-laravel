622145fb13d5a3826f92b99aa8f6c86b4dd220bf
Kofi Ramos Amoussou <karsoft92@gmail.com>
2020-04-19 19:39:05 +0000

    login page ui

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,21 +2,23 @@
 
 @section('content')
 
-<div class="flex  justify-center  bg-white mt-4 px-4 py-16">
-<form action="{{route('login')}}" method="post" class="px-8 py-4 mt-4 border-2 border-gray-400">
-   @csrf
-    <div class="w-full mt-4">
-      <label for="" class="block">Email</label>
-      <input type="text" name="email" id="" class="appearance-none mt-4">
-    </div>
-     <div class="w-full mt-4">
-      <label for="" class="block">Password</label>
-      <input type="password" name="password" id="" class="appearance-none focus:shadow-outline text-black focus:text-gray-600 mt-4">
-    </div>
-     <div class="w-full mt-4">
-     <button class="bg-black text-white rounded px-4 py-2">Login</button>
-    </div>
- </form>
+<div class="w-full">
+  <h3 class="text-4xl leading-10 w-full tracking-tight font-extrabold text-gray-900 text-center mt-4">Phoenix Mart</h3>
+  <div class="flex  justify-center  bg-white mt-4 bg-gray-50 px-4 mt-4">
+  <form action="{{route('login')}}" method="post" class="bg-gray-300 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+     @csrf
+      <div class="w-full mt-4">
+        <label for="" class="block text-gray-700 text-sm font-extrabold  text-xl  mb-2">Email</label>
+        <input type="text" name="email" id="" autofocus class=" mt-1 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username">
+      </div>
+       <div class="w-full mt-4">
+        <label for="" class="block text-gray-700 text-sm font-extrabold text-xl  mb-2">Password</label>
+        <input type="password" name="password" id="" class="mt-1 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+      </div>
+       <div class="w-full mt-4">
+       <button class="bg-black text-white rounded px-4 py-2">Login</button>
+      </div>
+   </form>
+  </div>
 </div>
-
 @endsection
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -19,7 +19,8 @@
                     Categories
                 </header>
                 @foreach($categories as $category)
-              <header class="font-bold text-xl flex uppercase"><a href="{{route('category',$category->name)}}" class="flex"><svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-6 h-6"><path d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>&nbsp;{{$category->name}}</a></header>
+              <header class="font-bold text-xl flex uppercase"><a href="{{route('category',$category->name)}}" class="flex"><svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-6 h-6"><path d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>&nbsp;{{$category->name}}</a>
+              </header>
 
                 @endforeach
 
